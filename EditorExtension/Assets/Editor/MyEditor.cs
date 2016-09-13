3c1947f7c7a6a93dba05e570288d54ab6b3774cd
y.seko <y.seko@yseko02-PC.local>
2016-09-13 19:01:47 +0900

    [add]カスタムウィンドウの追加
    追加

---
+++ EditorExtension/Assets/Editor/MyEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class MyEditor : MonoBehaviour {
+	[MenuItem("MyEditor/Open Window")]
+	static void OpenWindow() {
+		MyEditorWindow window = (MyEditorWindow)EditorWindow.GetWindow (typeof(MyEditorWindow));
+		window.Show ();
+	}
+}
+
+public class MyEditorWindow : EditorWindow {
+	void OnGUI() {
+		EditorGUILayout.LabelField ("My Editor");
+	}
+}
