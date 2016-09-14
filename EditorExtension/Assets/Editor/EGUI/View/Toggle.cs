1fb8a63832abf432e3d4a546897a70c31a25ac9b
y.seko <y.seko@yseko02-PC.local>
2016-09-14 17:27:07 +0900

    [add]トグルボタン追加

---
+++ EditorExtension/Assets/Editor/EGUI/View/Toggle.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace EGUI {
+	public class Toggle : Node {
+
+		public bool isOn;
+		public bool isLeft;
+		public GUILayoutOption[] options;
+
+		public override void Draw() {
+			if (isLeft) {
+				isOn = EditorGUILayout.ToggleLeft ("toggle", isOn, options);
+			}
+			else {
+				isOn = EditorGUILayout.Toggle ("toggle", isOn, options);
+			}
+			base.Draw ();
+		}
+	}
+}
