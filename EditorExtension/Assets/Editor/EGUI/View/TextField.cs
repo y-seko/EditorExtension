4eabef4eee819f9ec2c8fd2c724a7095e7b4917f
y.seko <y.seko@yseko02-PC.local>
2016-09-14 12:49:16 +0900

    [add]TextFieldクラス追加

---
+++ EditorExtension/Assets/Editor/EGUI/View/TextField.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace EGUI {
+	public class TextField : Node {
+		public string title;
+		public string text;
+		public GUILayoutOption[] options;
+
+		public TextField(string title, string text, params GUILayoutOption[] options) {
+			this.title = title;
+			this.text = text;
+			this.options = options;
+		}
+
+		public TextField(string title, string text) : this(title, text, null) {
+		}
+
+		public TextField(string text) : this("", text, null) {
+		}
+
+		public override void Draw() {
+			text = EditorGUILayout.TextField (title, text, options);
+			base.Draw ();
+		}
+	}
+}
