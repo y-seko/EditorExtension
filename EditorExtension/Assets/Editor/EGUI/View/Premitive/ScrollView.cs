bed0964be86a5ad9bd1ab29e83269bd8a10f43ca
y.seko <y.seko@yseko02-PC.local>
2016-09-20 12:50:24 +0900

    [add]スクロールビューを追加

---
+++ EditorExtension/Assets/Editor/EGUI/View/Premitive/ScrollView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+namespace EGUI {
+	public class ScrollView : BaseView {
+
+		Vector2 scrollPosition = Vector2.zero;
+
+		public ScrollView() : base () {
+		}
+
+		public override void Draw () {
+			scrollPosition = EditorGUILayout.BeginScrollView (scrollPosition);
+			base.Draw ();
+			EditorGUILayout.EndScrollView ();
+		}
+	}
+}
