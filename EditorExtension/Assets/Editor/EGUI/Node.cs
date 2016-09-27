1c8142141d8b078fc1e9b921d974fc7f55a01d2e
y.seko <y.seko@yseko02-PC.local>
2016-09-27 17:54:49 +0900

    [update]Node.csを実装

--- EditorExtension/Assets/Editor/EGUI/Node.cs
+++ EditorExtension/Assets/Editor/EGUI/Node.cs
@@ -1,10 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace EGUI {
 	/// <summary>
 	/// Node.
 	/// </summary>
 	public class Node {
+
+		Node parent;
+		List<Node> children;
+
+		public Node() {
+			children = new List<Node> ();
+		}
+
+		public Node(Node parent) : this() {
+			SetParent (parent);
+		}
+
+		/// <summary>
+		/// Sets the parent.
+		/// </summary>
+		/// <param name="parent">Parent.</param>
+		public void SetParent(Node parent) {
+			if (this.parent != null) {
+				this.parent.RemoveChild (this);
+				this.parent = null;
+			}
+			parent.AddChild (this);
+			this.parent = parent;
+		}
+
+		/// <summary>
+		/// Adds the child.
+		/// </summary>
+		/// <param name="child">Child.</param>
+		public void AddChild(Node child) {
+			child.parent = this;
+			children.Add (child);
+		}
+
+		/// <summary>
+		/// Removes the child.
+		/// </summary>
+		/// <param name="child">Child.</param>
+		public void RemoveChild(Node child) {
+			children.Remove (child);
+			child.parent = null;
+		}
 	}
 }
