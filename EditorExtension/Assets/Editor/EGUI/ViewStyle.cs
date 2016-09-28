d2475ab87034fcbb24c4f0b7c3f057aacf8cc31d
y.seko <y.seko@yseko02-PC.local>
2016-09-28 11:10:11 +0900

    [add]ビューのスタイルを定義

---
+++ EditorExtension/Assets/Editor/EGUI/ViewStyle.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace EGUI {
+	/// <summary>
+	/// View skin.
+	/// </summary>
+	public enum ViewStyle {
+		None,
+		Label,
+		Box,
+	}
+
+	static class ViewStyleExtension {
+		/// <summary>
+		/// Gets the GUI style.
+		/// </summary>
+		/// <returns>The GUI style.</returns>
+		/// <param name="skin">Skin.</param>
+		public static GUIStyle GetGUIStyle(this ViewStyle skin) {
+			switch (skin) {
+			case ViewStyle.Box:
+				return GUI.skin.box;
+			case ViewStyle.Label:
+				return GUI.skin.label;
+			default:
+				return GUIStyle.none;
+			}
+		}
+	}
+}
